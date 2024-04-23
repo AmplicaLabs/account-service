96009e485e5f818a47b7f8459b9bd69f748972b1
Matthew Orris <1466844+mattheworris@users.noreply.github.com>
2024-04-23 11:23:52 -0600

    feat: add apistos open api documenting

---
+++ rust-webhook-server/src/api.rs
@@ -0,0 +1,50 @@
+use actix_web::web::Json;
+use actix_web::Error;
+use apistos::actix::CreatedJson;
+use apistos::{api_operation, ApiComponent };
+use schemars::JsonSchema;
+use serde::{Deserialize, Serialize};
+
+#[derive(Serialize, JsonSchema, ApiComponent)]
+pub struct HealthResponse {
+    pub message: String,
+}
+
+#[derive(Deserialize, Serialize, JsonSchema, ApiComponent)]
+pub struct WebhookCallback {
+    pub reference_id: String,
+    pub address: String,
+    pub msa_id: String,
+    pub handle: String,
+}
+
+#[derive(Serialize, JsonSchema, ApiComponent)]
+pub struct EchoPayload {
+    pub reference_id: String,
+    pub address: String,
+    pub msa_id: String,
+    pub handle: String,
+}
+
+#[api_operation(summary = "Echo payload")]
+pub(crate) async fn echo_payload(body: Json<WebhookCallback>) -> Result<CreatedJson<EchoPayload>, Error> {
+    println!("Received payload: {}", serde_json::to_string_pretty(&body).unwrap());
+    let callback_msg = body.into_inner();
+    let payload = EchoPayload {
+        reference_id: callback_msg.reference_id,
+        address: callback_msg.address,
+        msa_id: callback_msg.msa_id,
+        handle: callback_msg.handle,
+    };
+    Ok(CreatedJson(payload))
+}
+
+pub(crate) async fn health_check() -> Result<Json<HealthResponse>, Error>{
+    println!("Health check");
+    Ok(Json(HealthResponse {
+        message: "Server is healthy".to_string(),
+    }))
+}
+
+
+
--- rust-webhook-server/src/main.rs
+++ rust-webhook-server/src/main.rs
@@ -1,23 +1,41 @@
-use actix_web::{web, App, HttpServer, Responder, HttpResponse};
+use crate::api::{echo_payload, health_check};
+use actix_web::middleware::Logger;
+use actix_web::{App, HttpServer};
+use apistos::app::OpenApiWrapper;
+use apistos::info::Info;
+use apistos::server::Server;
+use apistos::spec::Spec;
+use actix_web::web::{get, post, resource, scope};
+use std::error::Error;
+use std::net::Ipv4Addr;
 
-async fn echo_payload(info: web::Json<serde_json::Value>) -> impl Responder {
-    println!("Received payload: {}", info);
-    HttpResponse::Ok().json("Payload received")
-}
-
-async fn health_check() -> impl Responder {
-    HttpResponse::Ok().json("Server is healthy")
-}
+mod api;
 
 #[actix_web::main]
-async fn main() -> std::io::Result<()> {
-    println!("Starting Webhook Server...");
+async fn main() -> Result<(), impl Error> {
+    const HOST: Ipv4Addr = Ipv4Addr::new(127, 0, 0, 1);
+    const PORT: u16 = 5555;
+    println!("Starting Webhook Server on http://{}:{}", HOST, PORT);
     HttpServer::new(|| {
+        // let spec = Spec {
+        //     info: Info {
+        //         title: "Webhook Server".to_string(),
+        //         version: "1.0".to_string(),
+        //         description: Some("A simple webhook server".to_string()),
+        //         ..Default::default()
+        //     },
+        //     servers: vec![Server {
+        //         url: "/api/v3".to_string(),
+        //         ..Default::default()
+        //     }],
+        //     ..Default::default()
+        // };
+
         App::new()
-            .route("/webhook", web::post().to(echo_payload))
-            .route("/health", web::get().to(health_check))
+            .route("webhook", post().to(echo_payload))
+            .route("health", get().to(health_check))
     })
-    .bind("0.0.0.0:5555")?
+    .bind((HOST, PORT))?
     .run()
     .await
 }
